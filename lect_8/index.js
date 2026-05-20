a7c102daa73abdda44f882cfbf6d4f85c1c41fee
vartikadwivedi2005-code <vartikadwivedi2005@gmail.com>
2026-05-20 21:31:01 +0530

    TIC TAC TOE

--- lect_8/index.js
+++ lect_8/index.js
@@ -1,47 +1,65 @@
-let box=document.querySelectorAll("button")
- let resetBtn=  document.querySelector("#reset")
- let msgBox=  document.querySelector("h2")
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset");
+let msgBox = document.querySelector("#msg");
 
- let win=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[0,4,8],[2,4,6],[2,5,8]]
- let turn0=true
- for(let a of box){
-    a.addEventListener("click",()=>{
-        console.log("hello");
-        if(turn0){
-            a.innerText="0"
-            turn0=false
-            a.disabled=true
-            // checkWinner()
-        }else{
-            a.innerText="X"
-            turn0=true
-            a.disabled=true
-            checkWinner()
 
+const winPatterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+    [0, 4, 8], [2, 4, 6]             
+];
+
+let turn0 = true;
+
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (turn0) {
+            box.innerText = "0";
+            box.style.color = "#5c3d59"; 
+            turn0 = false;
+        } else {
+            box.innerText = "X";
+            box.style.color = "#7209b7"; 
+            turn0 = true;
         }
-        resetBtn.addEventListener("click",()=>{
-            for(let a of box){
-                a.innerText=""
-                a.disabled=false
-            }
-            msgBox.innerText=""
-        })
-    })
-   
- }
+        
+        box.disabled = true;
+        checkWinner();
+    });
+});
 
- function checkWinner(){
-    for(let pattern of win){
-     let x =  box[pattern[0]].innerText
-    let y  =  box[pattern[1]].innerText
-    let z  =  box[pattern[2]].innerText
-    if(x!="" && y!="" && z!=""){
-        if(x==y && y==z){
-            msgBox.innerText=  x + " wins!"
-            console.log("kya hua");
-            
-          }
+
+function checkWinner() {
+    for (let pattern of winPatterns) {
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+
+        if (pos1 !== "" && pos2 !== "" && pos3 !== "") {
+            if (pos1 === pos2 && pos2 === pos3) {
+                showWinner(pos1);
+                return; 
+            }
+        }
     }
-    }
- }
+}
+
+
+function showWinner(winner) {
+    msgBox.innerText = `🎉 Player ${winner} Wins!`;
+    
+    
+    boxes.forEach(box => box.disabled = true);
+}
+
+
+resetBtn.addEventListener("click", () => {
+    boxes.forEach((box) => {
+        box.innerText = "";
+        box.disabled = false;
+    });
+    msgBox.innerText = "";
+    turn0 = true;
+});
 
