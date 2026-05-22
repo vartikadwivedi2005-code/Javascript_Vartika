aea43dac735feb2312dfe0e341d6b04e36fadf5f
vartikadwivedi2005-code <vartikadwivedi2005@gmail.com>
2026-05-22 23:16:27 +0530

    lecture

---
+++ lect_26/index.js
@@ -0,0 +1,9 @@
+let a = 10;
+
+function sum(num1, num2) {
+    console.log(num1 + num2);
+}
+
+// Now you can call them right here in the console:
+console.log(a);    // Outputs: 10
+sum(5, 5);         // Outputs: 10
