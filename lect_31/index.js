054d8b00ff37f4be50d2ad6b737913844cfc80a7
vartikadwivedi2005-code <vartikadwivedi2005@gmail.com>
2026-06-07 22:36:23 +0530

    Operators

--- lect_31/index.js
+++ lect_31/index.js
@@ -5,6 +5,7 @@
 // console.log(5 / 10); // 0.5
 // console.log(5 % 10); // 5 (modulus operator gives the remainder of the division)
 
+
 // // Assignment Operators
 // let x = 5;
 // x += 10; // x = x + 10; x is now 15
@@ -12,6 +13,7 @@
 // x *= 2; // x = x * 2; x is now 20
 // x /= 4; // x = x / 4; x is now 5
 // x %= 3; // x = x % 3; x is now 2
+
 
 // // Comparison Operators
 // console.log(5 == "5"); // true (loose equality, checks value only)
@@ -24,7 +26,6 @@
 // console.log(5 <= 4); // false
 // console.log(5==3); // false
 // console.log(5!=3); // true
-
 
 
 // let z = "10";
@@ -40,7 +41,9 @@
 // console.log(b);
 // console.log(typeof b);
 
+
 // console.log(0 / 0); // This will also result in NaN because division of zero by zero is undefined in mathematics.
+
 
 // Number -> String
 // let a = 10;
@@ -49,12 +52,12 @@
 // console.log(typeof b);
 
 
-
 // let a = true;
 // console.log(a); // true
 // console.log(typeof a); // boolean
 // console.log(Number(true)); // 1 (when converting true to a number, it becomes 1)
 // console.log(String(true)); // "true" (when converting true to a string, it becomes "true")
+
 
 // let b = false;
 // console.log(b); // false
@@ -63,9 +66,9 @@
 // console.log(String(false)); // "false" (when converting false to a string, it becomes "false")
 
 
-console.log(Number(null)); // 0 (when converting null to a number, it becomes 0)
-console.log(String(null)); // "null" (when converting null to a string, it becomes "null")
+// console.log(Number(null)); // 0 (when converting null to a number, it becomes 0)
+// console.log(String(null)); // "null" (when converting null to a string, it becomes "null")
 
-console.log(Number(undefined)); // NaN (when converting undefined to a number, it becomes NaN)
-console.log(String(undefined)); // "undefined" (when converting undefined to a string, it becomes "undefined")
+// console.log(Number(undefined)); // NaN (when converting undefined to a number, it becomes NaN)
+// console.log(String(undefined)); // "undefined" (when converting undefined to a string, it becomes "undefined")
 
